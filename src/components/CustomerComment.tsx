3b6efa6d6fc4e3712e1002e4ce721c8813663f70
Hassan Mahadjir <hm.mahadjir@gmail.com>
2025-08-03 15:40:40 +0300

    customer comments

--- src/components/CustomerComment.tsx
+++ src/components/CustomerComment.tsx
@@ -1,28 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Carousel from "./Cards-Carousel";
 import { useTranslations } from "next-intl";
-
-const comments = [
-  {
-    name: "Ahmed Al-Saud",
-    comment:
-      "Super Shield has transformed my driving experience! The heat insulation is incredible, and I can feel the difference immediately.",
-    rating: 5,
-  },
-  {
-    name: "Fatima Al-Harbi",
-    comment:
-      "Excellent product, easy to install and very effective against heat.",
-    rating: 4,
-  },
-  {
-    name: "Mohammed Al-Otaibi",
-    comment: "I recommend Super Shield to everyone. Great value for money!",
-    rating: 5,
-  },
-];
+import { supabase } from "../lib/supabseClient";
 
 const CustomerComment = () => {
+  type Comment = {
+    name: string;
+    comment: string;
+    rating: number;
+  };
+  const [comments, setComments] = useState<Comment[]>([]);
+  useEffect(() => {
+    const fetchComments = async () => {
+      const { data, error } = await supabase.from("comments").select("*");
+      console.log(data);
+      if (error) {
+        console.error("Error fetching comments:", error);
+        setComments([]);
+      } else {
+        setComments(data);
+      }
+    };
+    fetchComments();
+  }, []);
+
   const t = useTranslations("common");
 
   return (
